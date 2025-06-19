94e55352ab537faf5cea42a2629eb78875942e5f
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-06-19 16:14:55 +0400

    created basic login simulation with redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { login, logout, setUserName } from "./features/authSlice";
+import "./App.css";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const username = useSelector((state) => state.auth.username);
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    dispatch(setUserName(userInput));
+    dispatch(login());
+    setUserInput("");
+  }
+
   return (
-   <div></div>
-  )
+    <div>
+      <h2>{isLoggedIn ? `Hello ${username}` : "Hello stranger"}</h2>
+
+      {!isLoggedIn && (
+        <>
+          <input
+            type="text"
+            placeholder="Enter username"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <button onClick={handleLogin}>Log in</button>
+        </>
+      )}
+
+      {isLoggedIn && (
+        <button onClick={() => dispatch(logout())}>Log out</button>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
